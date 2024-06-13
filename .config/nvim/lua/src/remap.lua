a19ca5c6bbf0c22ea7648fef539cbc1c75c0464b
dobrotacreator <igor.p.i@yandex.ru>
2024-06-13 19:14:43 +0300

    improve movement

--- .config/nvim/lua/src/remap.lua
+++ .config/nvim/lua/src/remap.lua
@@ -19,18 +19,35 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-vim.keymap.set("n", "<leader>ed", vim.diagnostic.open_float)
+vim.keymap.set("n", "<leader>sd", vim.diagnostic.open_float)
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
 
 vim.keymap.set("n", "<leader>sc", function()
 	vim.opt.spell = not (vim.opt.spell:get())
 end)
 
--- local camel_char = "A-Z0-9.,;:{([`'\""
--- vim.keymap.set({ "n", "v" }, "<leader>w", [[<cmd>call search('\C\<\<Bar>\%(^\<Bar>[^'.g:camelchar.']\@<=\)['.g:camelchar.']\<Bar>['.g:camelchar.']\ze\%([^'.g:camelchar.']\&\>\@!\)\<Bar>\%^','bW')<CR>]])
--- nnoremap <silent><C-Left> :<C-u>call search('\C\<\<Bar>\%(^\<Bar>[^'.g:camelchar.']\@<=\)['.g:camelchar.']\<Bar>['.g:camelchar.']\ze\%([^'.g:camelchar.']\&\>\@!\)\<Bar>\%^','bW')<CR>
--- nnoremap <silent><C-Right> :<C-u>call search('\C\<\<Bar>\%(^\<Bar>[^'.g:camelchar.']\@<=\)['.g:camelchar.']\<Bar>['.g:camelchar.']\ze\%([^'.g:camelchar.']\&\>\@!\)\<Bar>\%$','W')<CR>
--- inoremap <silent><C-Left> <C-o>:call search('\C\<\<Bar>\%(^\<Bar>[^'.g:camelchar.']\@<=\)['.g:camelchar.']\<Bar>['.g:camelchar.']\ze\%([^'.g:camelchar.']\&\>\@!\)\<Bar>\%^','bW')<CR>
--- inoremap <silent><C-Right> <C-o>:call search('\C\<\<Bar>\%(^\<Bar>[^'.g:camelchar.']\@<=\)['.g:camelchar.']\<Bar>['.g:camelchar.']\ze\%([^'.g:camelchar.']\&\>\@!\)\<Bar>\%$','W')<CR>
--- vnoremap <silent><C-Left> :<C-U>call search('\C\<\<Bar>\%(^\<Bar>[^'.g:camelchar.']\@<=\)['.g:camelchar.']\<Bar>['.g:camelchar.']\ze\%([^'.g:camelchar.']\&\>\@!\)\<Bar>\%^','bW')<CR>v`>o
--- vnoremap <silent><C-Right> <Esc>`>:<C-U>call search('\C\<\<Bar>\%(^\<Bar>[^'.g:camelchar.']\@<=\)['.g:camelchar.']\<Bar>['.g:camelchar.']\ze\%([^'.g:camelchar.']\&\>\@!\)\<Bar>\%$','W')<CR>v`<o
+vim.keymap.set("v", "<leader>c", function()
+	local start_pos = vim.fn.getpos("v")
+	local end_pos = vim.fn.getpos(".")
+
+	local start_row, start_col = start_pos[2] - 1, start_pos[3] - 1
+	local end_row, end_col = end_pos[2] - 1, end_pos[3] - 1
+	if start_col > end_col then
+		start_row, start_col, end_row, end_col = end_row, end_col, start_row, start_col
+	end
+
+	local lines = vim.api.nvim_buf_get_text(0, start_row, start_col, end_row, end_col, {})
+	local selected_text = table.concat(lines, "\n")
+
+	if selected_text:find("[a-z][A-Z]") then
+		local snake_case_text = selected_text:gsub("([a-z])([A-Z])", "%1_%2"):lower()
+		vim.api.nvim_buf_set_text(0, start_row, start_col, end_row, end_col, { snake_case_text })
+	elseif selected_text:find("_[a-z]") then
+		local camel_case_text = selected_text:gsub("(_)([a-z])", function(_, l)
+			return l:upper()
+		end)
+		vim.api.nvim_buf_set_text(0, start_row, start_col, end_row, end_col, { camel_case_text })
+	else
+		print("Not a snake_case or camelCase word")
+	end
+end)
