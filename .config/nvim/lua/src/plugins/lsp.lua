b86a64cc98be6f0d640191f8bae3c80ae8fcbb1f
dobrotacreator <igor.p.i@yandex.ru>
2024-06-14 17:37:56 +0300

    ruff code actions

--- .config/nvim/lua/src/plugins/lsp.lua
+++ .config/nvim/lua/src/plugins/lsp.lua
@@ -54,6 +54,33 @@ return {
 					})
 				end
 
+				if client and client.name == "ruff_lsp" then
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						callback = function()
+							if vim.bo.ft == "python" then
+								vim.lsp.buf.code_action({
+									context = {
+										diagnostics = {},
+										only = { "source.fixAll.ruff" },
+									},
+									apply = true,
+								})
+							end
+						end,
+					})
+					map("<space>ca", function()
+						vim.lsp.buf.code_action({
+							context = {
+								diagnostics = {},
+								only = {
+									"source.organizeImports",
+								},
+							},
+							apply = true,
+						})
+					end)
+				end
+
 				if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
 					map("<leader>ih", function()
 						vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
